ef7f92f724fdebf587e06a582dc4841810be2b43
Francisco Artur Barboza <multivalue.fbarboza@terceiros.mag.com.br>
2023-03-23 07:27:01 -0300

    Adicionando arquivos do Projeto Python, Segurado e Beneficiario

---
+++ Projeto00/Beneficiario.py
@@ -0,0 +1,9 @@
+class Beneficiario:
+    def __init__(self, nome, DataNascimento, cpf, rg, tipo, endereco, contato):
+        self.nome = nome
+        self.DataNascimento = DataNascimento
+        self.Cpf = cpf
+        self.RG = rg
+        self.Endereco = endereco
+        self.Contato = contato
+        self.Tipo = tipo
---
+++ Projeto00/segurado.py
@@ -0,0 +1,10 @@
+class Segurado:
+   def __init__(self,nome,DataNascimento,cpf,rg,endereco,contato,beneficiarios):
+       self.nome =  nome
+       self.DataNascimento = DataNascimento
+       self.Cpf = cpf
+       self.RG = rg
+       self.Endereco = endereco
+       self.Contato = contato
+       self.Beneficiarios = beneficiarios
+
