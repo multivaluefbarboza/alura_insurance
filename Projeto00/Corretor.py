1a28f32563965ce89f3b7c6094cfbcbe7de13a59
Francisco Artur Barboza <multivalue.fbarboza@terceiros.mag.com.br>
2023-03-23 07:35:45 -0300

    Adicionado classe corretor

---
+++ Projeto00/Corretor.py
@@ -0,0 +1,7 @@
+class Corretor:
+    def __init__(self,Nome,NumeroSusep,Apolices,contato):
+        self.Nome = Nome
+        self.NumeroSusep = NumeroSusep
+        self.Apolices = Apolices
+        self.Contato = contato
+    
