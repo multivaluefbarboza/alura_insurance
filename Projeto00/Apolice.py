37c483e02c227da77696404c6904197ded3862e5
Francisco Artur Barboza <multivalue.fbarboza@terceiros.mag.com.br>
2023-03-23 07:32:55 -0300

    Adicionando classe Apolice

---
+++ Projeto00/Apolice.py
@@ -0,0 +1,10 @@
+class Apolice:
+    def __init__(self,Numero,Tipo,ValorPremio,Segurado,Corretor,Vigencia,DataCriacao,Status):
+        self.Numero = Numero
+        self.Tipo = Tipo
+        self.ValorPremio = ValorPremio
+        self.Segurado = Segurado
+        self.Corretor = Corretor
+        self.Vigencia = Vigencia
+        self.DataCriacao = DataCriacao
+        self.Status = Status
